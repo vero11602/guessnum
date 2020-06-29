2f8a5f3ba31f1d20310a7c534beb8fe74958ab9f
Ingrid Jiang <vero11602@gmail.com>
2020-06-29 18:09:04 +0800

    add range

--- guessnum.py
+++ guessnum.py
@@ -1,6 +1,10 @@
 import random
-r = random.randint (1 , 100)
 count = 0
+low = input ('請決定數值最小範圍:')
+low = int (low)
+high = input ('請決定數值最大範圍:')
+high = int (high)
+r = random.randint (low , high)
 while True :
 	count+=1 #count = count + 1 
 	num = input ('請輸入數字:')
